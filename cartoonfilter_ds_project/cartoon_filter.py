369e818d4e0da9ff25743d224244399867e4698c
TverskayN <62351122+TverskayN@users.noreply.github.com>
2020-10-03 23:59:30 +0500

    Add cartoon_filter (#2)
    * Add cartoon_filter
    
    * Added an exception and removed the extra print
    
    * Исправила замечания

---
+++ cartoonfilter_ds_project/cartoon_filter.py
@@ -0,0 +1,58 @@
+import cv2
+
+
+def apply_cartoon_filter(img_dir, save_img_dir):
+    """
+    Из фотографии создает мультяшное изображение и выводит его.
+    На вход получает путь к исходному изображению.
+    
+    Изменение изображение делится на 5 шагов:
+    1. Примените bilateral filter (цветовой фильтр), чтобы уменьшить цветовую палитру изображения.
+    2. Преобразуйте исходное цветное изображение в grayscale (оттенки серого).
+    3. Примените median blur (медиальное размытие), чтобы уменьшить шум изображения.
+    4. Используйте adaptive thresholding (адаптивное пороговое значение) для обнаружения и выделения ребер в маске ребер.
+    5. Объедините цветное изображение из шага 1 с краевой маской из шага 4.
+    """
+    # Считывает данные из файла и переводит в различные форматы
+    img = cv2.imread(img_dir)
+    
+    josh_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    josh_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    josh_gray = cv2.cvtColor(josh_rgb, cv2.COLOR_RGB2GRAY)
+        
+    # Количество шагов снижения (экземпляров)
+    numDownSamples = 2
+    # Количество шагов двусторонней фильтрации
+    numBilateralFilters = 7
+    # Первый шаг
+    # Нисходящее изображение с использованием Гауссовой пирамиды 
+    for _ in range(numDownSamples):
+        josh_rgb = cv2.pyrDown(josh_rgb)
+    # Повторно применяем небольшой двусторонний фильтр 
+    for _ in range(numBilateralFilters):
+        josh_rgb = cv2.bilateralFilter(josh_rgb, 9, 9, 7)
+    # Вверх по образцу изображения до исходного размера
+    for _ in range(numDownSamples):
+        josh_rgb = cv2.pyrUp(josh_rgb)
+    # Комбинируем шаги шаги 2 и 3
+    # Преобразование в оттенки серого и применение медианного размытия
+    josh_blur = cv2.medianBlur(josh_gray, 7)
+    # Четвертый шаг
+    # Обнаружение и усиление краев
+    img_edge = cv2.adaptiveThreshold(josh_blur, 255,
+                                    cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 2)
+    # Пятый шаг
+    # Объедините цветное изображение из шага 1 с краевой маской из шага 4.
+    img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
+    try:
+        img_result = cv2.bitwise_and(josh_rgb, img_edge)
+    except cv2.error:
+        raise TypeError("Changed the photo")
+    else:
+        img_result = cv2.cvtColor(img_result, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(save_img_dir, img_result)
+        return "Ok"
+        
+        
+    
+    
--- cartoonfilter_ds_project/handlers.py
+++ cartoonfilter_ds_project/handlers.py
@@ -3,6 +3,8 @@ import os
 from telegram.ext import Updater, CommandHandler, MessageHandler
 from telegram import ReplyKeyboardMarkup
 
+from cartoon_filter import apply_cartoon_filter
+
 def main_keyboard():
     return ReplyKeyboardMarkup([
         ['Neural network'],['Cartoon filter']
@@ -31,14 +33,19 @@ def greet_user(update, context):
 def cartoonify(update, context):
     #получаем фото в самом хорошем качестве
     photo = context.bot.getFile(update.message.photo[-1].file_id)
+    # Сохраняем фото с названием photo.jpg в папку downloads
+    os.makedirs("downloads", exist_ok=True)
+    file_name = os.path.join("downloads", "photo.jpg")
+    photo.download(file_name)
+    update.message.reply_text("The photo is saved")
     # меняем фотку с помощью нейросетки
     if context.user_data['filter_type'] == 'Neural network':
        cartoonize_using_neuro()
        update.message.reply_text('It\'s working!')
     #меняем фотку с помощью фильра, над которым сейчас работает наташа 
     elif context.user_data['filter_type'] == 'Cartoon filter':
-        cartoonise_using_cartoonfilter()
-        update.message.reply_text('It\'s working!22')
+        cartoonise_using_cartoonfilter(update, context, file_name)
+        
     #это пока не работает, пока не знаю почему
     else:
         print(33)
@@ -50,6 +57,24 @@ def cartoonize_using_neuro():
     print(1)
 
 #функция для фильтров
-def cartoonise_using_cartoonfilter():
-    print(2)
+def cartoonise_using_cartoonfilter(update, context, file_name):
+    update.message.reply_text("Processing the photo")
+    
+    # Сохраняем обработанное фото под именем cartoon_photo.jpg в папку downloads. 
+    # Используется полное имя
+    cartoon_file_name = os.path.abspath(os.path.join("downloads", "cartoon_photo.jpg"))
+
+    # Если модуль apply_cartoon_filter выдал ошибку, то отправляем пользователю сообщение.
+    try:
+        apply_cartoon_filter(os.path.abspath(file_name), cartoon_file_name)
+    except TypeError:
+        update.message.reply_text("I can't process this photo. Choose another one")
+    else:
+        # Вывод обработанного фото пользователю, если модуль отработал без ошибок.
+        chat_id = update.effective_chat.id
+        context.bot.send_photo(chat_id=chat_id, photo=open(cartoon_file_name, 'rb'))
+    
+    
+
+    
 
