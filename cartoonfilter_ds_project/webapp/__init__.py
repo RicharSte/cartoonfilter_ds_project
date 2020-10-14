3cdf1c69397d6f7d4dd9ebff6c96ffbec2cf81dc
TverskayN <tverskayn@gmail.com>
2020-10-14 20:11:12 +0500

    Промежуточный вариант3

--- cartoonfilter_ds_project/webapp/__init__.py
+++ cartoonfilter_ds_project/webapp/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, request, render_template, flash, redirect, url_for
+from werkzeug.utils import secure_filename
 
 from webapp.forms import FileForm
 
@@ -6,24 +7,42 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
 
-    @app.route('/', methods=['POST', 'GET'])
+    def allowed_file(filename):
+        # Проверяет есть ли  расширение файла в списке разрешенных расширений
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    @app.route('/', methods=['GET', 'POST'])
     def index():
         title = "Обработчик фотографий"
         file_form = FileForm()
+        if request.method == 'POST':
+            # Проверка есть ли файл в запросе
+            if 'file' not in request.files:
+                flash('Нет файла')
+                return redirect(request.url)
+            # кладем файл из запроса в file
+            file = request.files['file']
+            # Если пользователь не выбирает файл, 
+            # браузер может отправить пустую часть без имени файла
+            if file.filename == '':
+                flash("Файл не выбран")
+                return redirect(request.url)
+            # Если расширение файла в списке разрешенных
+            if file and allowed_file(filename):
+                # проверка безопасности имени файла
+                filename = secure_filename(file.filename)
+                # Сохранение файла
+                file.save(os.path.abspath(os.path.join("downloads", filename)))
+
         if file_form.validate_on_submit(): # если не возникло ошибок при заполнении формы
             flash('Ок')
-            #file_form.photo.data
-            # Защита имени файла
-            filename = secure_filename(photo_file.filename)
-            # Сохранение фото
-            photo.save(os.path.join(app.instance_path, 'photos', filename))
 
             if request.file_form['processing'] == 'cortoon_filter': # обработка фильтрами
                 flash('Обработка фильтрами')
                 return redirect(url_for('photo_processing'))
             elif request.file_form['processing'] == 'neural_network': # обработка ИИ
                 flash('Обработка ИИ')
-            return redirect(url_for('index'))
+            return redirect(url_for('photo_processing'))
         return render_template('index.html', title=title, form=file_form)
 
     
