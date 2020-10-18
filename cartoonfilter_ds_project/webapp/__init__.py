9afa369d5c4060bb14785d5a91bed1aaf6d0891d
Stepabov Nikolai <Data.Science.NikSte@yandex.ru>
2020-10-18 22:06:55 +0300

    Сайт теперь умеет обрабатывать изображения и сохранять их

--- cartoonfilter_ds_project/webapp/__init__.py
+++ cartoonfilter_ds_project/webapp/__init__.py
@@ -1,16 +1,16 @@
-import os
+from flask import Flask, request, render_template, flash, redirect, url_for 
+from PIL import Image
+from skimage import io as stikIO
+from werkzeug.utils import secure_filename 
 
-from flask import Flask, request, render_template, flash, redirect, url_for
-from io import StringIO
-from werkzeug.utils import secure_filename
-
+from cartoonise_using_cartoonfilter import cartoonise_using_cartoonfilter
+from cartoonize_using_network_without_filters import cartoonize_using_network_without_filters
 from webapp.forms import FileForm
-
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-
+    
     def allowed_file(filename):
         # Проверяет есть ли  расширение файла в списке разрешенных расширений
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg'}
@@ -34,17 +34,31 @@ def create_app():
             if file and allowed_file(file.filename):
                 # проверка безопасности имени файла
                 filename = secure_filename(file.filename)
-                file_in_bytes = file.read()
-                return file_in_bytes
 
         if file_form.validate_on_submit(): # если не возникло ошибок при заполнении формы
             flash('Ок')
+            #считываем картинку сразу конвентируя информацию в ndarray
+            file_in_ndarray = stikIO.imread(file)
 
             if file_form.processing.data == 'cartoon_filter': # обработка фильтрами
                 flash('Обработка фильтрами')
+                try:
+                    photo = cartoonise_using_cartoonfilter(file_in_ndarray) 
+                    photo = Image.open(photo)
+                #скачиваем фото
+                    photo.save('downloads\photo.jpeg')          
+                except TypeError:
+                    #АХТУНГ это наддо выводить на экран пользователю, если не возможно обработать фото
+                    print('это фото невозможно обработать, выберите другое')
                 return redirect(url_for('photo_processing'))
+            
             elif file_form.processing.data == 'neural_network': # обработка ИИ
                 flash('Обработка ИИ')
+            #обрабатываем фото, на выходе данные находятся в формате _io.BytesIO
+                photo = cartoonize_using_network_without_filters(file_in_ndarray)
+                photo = Image.open(photo)
+            #скачиваем фото
+                photo.save('downloads\photo.jpeg')
             return redirect(url_for('photo_processing'))
         return render_template('index.html', title=title, form=file_form)
     
--- cartoonfilter_ds_project/webapp/config.py
+++ cartoonfilter_ds_project/webapp/config.py
@@ -1,2 +1,2 @@
-SECRET_KEY = ''
+SECRET_KEY = 'bctiuwgnyh7w5n6yh79rn97bm4c7r9'
 ALLOWED_EXTENSIONS = {'jpg'}
