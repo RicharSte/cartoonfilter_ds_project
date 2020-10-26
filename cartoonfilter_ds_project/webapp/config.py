9e146b81e1366ab579af23e3e00bd60ca0e5e060
Nikolai St <63543710+RicharSte@users.noreply.github.com>
2020-10-26 11:09:43 +0300

    Webapp3 (#10)
    * Fix imports
    
    * Убрал сохранение объектов на жесткий диск. Считываем биты в переменную
    
    * Поправил строки
    
    * Разделил обработчики на отдельные файлы, переработал Handlers
    
    * Сайт теперь умеет обрабатывать изображения и сохранять их
    
    * Добавленна авторизация
    
    * Сделал аторизацию пользователей пользователей
    
    * РЕшил вопрос с requirements
    
    * Решил вопрос с requirements
    
    * Теперь это работает на 3.8.5

--- cartoonfilter_ds_project/webapp/config.py
+++ cartoonfilter_ds_project/webapp/config.py
@@ -1,2 +1,9 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 SECRET_KEY = ''
 ALLOWED_EXTENSIONS = {'jpg'}
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
