fc619e5638a335d1ab05e78a6c848b6d10f07862
Stepabov Nikolai <Data.Science.NikSte@yandex.ru>
2020-11-02 22:00:37 +0300

    Решена проблемма с запоздалым отображением фото

--- cartoonfilter_ds_project/webapp/__init__.py
+++ cartoonfilter_ds_project/webapp/__init__.py
@@ -1,4 +1,5 @@
 import os
+from random import randint
 
 from flask import Flask, request, render_template, flash, redirect, url_for 
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
@@ -12,9 +13,9 @@ from webapp.forms import FileForm, LoginForm
 from webapp.model import db, User
 
 PATH_TO_DOWNLOADS = os.path.join(
-os.path.abspath(os.path.dirname(__file__)), 
-'static', 'images', 'downloads', 'photo.jpeg'
-)
+os.path.abspath(os.path.dirname(__file__)), 'static', 'images', 'downloads')
+
+RANDOM_NAME = ''
 
 def create_app():
     app = Flask(__name__)
@@ -65,8 +66,12 @@ def create_app():
                 try:
                     photo = cartoonise_using_cartoonfilter(file_in_ndarray) 
                     photo = Image.open(photo)
+                #даем случайное имя файлу, чтобы отправить его пользовате    
+                    random_name = str(randint(0, 10000))+'.jpeg'
+                    global RANDOM_NAME
+                    RANDOM_NAME = random_name
                 #скачиваем фотo
-                    photo.save(PATH_TO_DOWNLOADS)          
+                    photo.save(os.path.join(PATH_TO_DOWNLOADS, RANDOM_NAME))         
 
                 except TypeError:
                     # Если невозможно обработать фото, то пользователь видит
@@ -79,9 +84,11 @@ def create_app():
             #обрабатываем фото, на выходе данные находятся в формате _io.BytesIO
                 photo = cartoonize_using_network_without_filters(file_in_ndarray)
                 photo = Image.open(photo)
-            #скачиваем фото
-
-                photo.save(PATH_TO_DOWNLOADS)         
+            #даем случайное имя файлу, чтобы отправить его пользователю
+                random_name = str(randint(0, 10000))+'.jpeg'
+                RANDOM_NAME = random_name
+            #сохраняем файл
+                photo.save(os.path.join(PATH_TO_DOWNLOADS, RANDOM_NAME))         
             return redirect(url_for('photo_processing'))
 
         name_photo_example = ['liuyifei4.jpg', 'mountain4.jpg', 'photo1_cartoon.jpg',
@@ -95,7 +102,7 @@ def create_app():
     def login():
         if current_user.is_authenticated:
             return redirect(url_for('index'))
-        title = 'Аторизация'
+        title = 'Авторизация'
         login_form = LoginForm()
         return render_template('login.html', page_title=title, form=login_form)
 
@@ -123,17 +130,5 @@ def create_app():
     @app.route('/photo')
     def photo_processing():
         title = "Вот такое фото получилось"
-        path_photo = os.path.join('static', 'images', 'downloads', 'photo.jpeg')
+        path_photo = os.path.join('static', 'images', 'downloads', RANDOM_NAME)
         return render_template('photo.html', title=title, path_photo=path_photo)
-
-
-#идея в том чтобы для не зарегистрированных пользователей не была доступна возможность 
-#отбрабатывать фотки на сайте. Т.е. нужно дописать Главную страницу, которая будет показываться всем
-#а после регистрации была страница с обработчиком фото (это можно передать наташе, пока я займусь S3)     
-    @login_required
-    def func():
-        pass
-    
-    
-    return app
-
